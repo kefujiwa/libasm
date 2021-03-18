d12c3a070201528903f99e6994675f4471a1b567
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-19 00:07:19 +0900

    fix(main.c): add test case of ft_strlen.s
    - ft_strlen.s用のテストケースを追加

--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:25:33 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/03/18 23:27:36 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/03/19 00:03:04 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,6 +115,26 @@ void	read_test_err(int fd, char *buf, int size)
 	printf("ft_read [%d] : errno > %d\n\n", ret[1], err[1]);
 }
 
+void	strlen_test(char *str)
+{
+	int	ret[2];
+	int	err[2];
+
+	errno = 0;
+	printf("-----strlen(%s)-----\n", str);
+	ret[0] = strlen(str);
+	err[0] = errno;
+	ret[1] = ft_strlen(str);
+	err[1] = errno;
+	if (ret[0] == ret[1])
+		printf("" GREEN "[OK]" RESET "\n");
+	else
+		printf("" RED "[KO]" RESET "\n");
+	printf("strlen    [%d] : errno > %d\n", ret[0], err[0]);
+	printf("ft_strlen [%d] : errno > %d\n\n", ret[1], err[1]);
+}
+
+
 int		main(void)
 {
 	/*
@@ -148,4 +168,16 @@ int		main(void)
 	read_test_err(0, NULL, BUFFER_SIZE);
 	read_test_err(0, "buf", -1);
 	read_test_err(-1, NULL, -1);
+
+	
+	/*
+	** FT_STRLEN
+	*/
+	printf("\n<<<<<<<<<<<< ft_strlen.s >>>>>>>>>>>>\n");
+	strlen_test("42");
+	strlen_test("");
+	strlen_test("Done is better than perfect");
+	strlen_test("          ");
+	strlen_test("hey yo\0aaaaaaaa");
+	strlen_test("To be or not to be, that is the question. The original meaning of this character was like NOP—when sent to a printer or a terminal, it does nothing (some terminals, however, incorrectly display it as space).");
 }
