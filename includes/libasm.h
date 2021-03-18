8faf92ed7221c3f5506196e91e6a13650c4cdabe
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-18 20:51:00 +0900

    feat(libasm.h): create header file for libasm
    - ヘッダファイル作成

---
+++ includes/libasm.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/18 18:19:59 by kefujiwa          #+#    #+#             */
+/*   Updated: 2021/03/18 19:00:23 by kefujiwa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBASM_H
+# define LIBASM_H
+
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# define RESET	"\033[0m"
+# define RED	"\033[31m"
+# define GREEN	"\033[32m"
+
+# define BUFFER_SIZE 1024
+
+ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
+
+#endif
