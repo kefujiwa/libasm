6528cca53bab237dd87f0e17409f9bb3048367a9
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-20 02:08:08 +0900

    fix(ft_write.s): delete useless processes; modify condition of error handler
    - syscall後にキャリーフラグが立っていたらエラー処理するという形に修正
    - 上記修正に伴い、不要な処理を全て削除

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -20,48 +20,16 @@ extern ___error
 
 section .text
 _ft_write:
-	push	r8				; store data of r8 in stack
-	push	r9				; store data of r9 in stack
-
-	mov		r8, rsi			; store data of rsi (buf) in r8 before fstat
-	mov		r9, rdx			; store data of rdx (nbyte) in r9 before fstat
-	mov		rsi, 0x0		; setting NULL pointer to the second parameter before fstat
-	mov		rax, 0x20000BD	; 0x2000000 (MacOS ?) + 0xBD (fstat syscall)
-	syscall					; fstat(rdi, rsi)
-	cmp		rax, 9			; check if fstat returned 9 (errno 9:EBADF)
-	je		_error_fd		; jump to _error_fd if ZF == 1
-
-	mov		rsi, r8			; restore buf in rsi
-	cmp		rsi, 0x0		; check if rsi (buf) is NULL pointer
-	je		_error_addr		; jump to _error_addr if ZF == 1
-
-	mov		rdx, r9			; restore nbyte in rdx
 	mov		rax, 0x2000004	; 0x2000000 (MacOS ?) + 0x4 (write syscall)
 	syscall					; write(rdi, rsi, rdx)
-	cmp		rax, 22			; check if write returned 22 (errno 22:EINVAL)
-	je		_error_args		; jump to _success if ZF == 0
-
-_return:
-	pop		r8				; restore data of r8
-	pop		r9				; restore data of r9
+	jc		_error			; jump to _success if CF == 1
 	ret
 
-_error_fd:
+_error:
+	push	r8				; store data of r8 in stack
+	mov		r8, rax			; copy rax (return value) to r8
 	call	___error		; store ptr of variable errno to rax
-	mov		byte [rax], 9	; assign 9:EBADF to errno
-	mov		rax, -1			; return value -1
-	jmp		_return
-
-_error_addr:
-	call	___error		; store ptr of variable errno to rax
-	mov		byte [rax], 14	; assign 14:EFAULT to errno
-	mov		rax, -1			; return value -1
-	jmp		_return
-
-_error_args:
-	cmp		rdx, 22			; check if third parameter is 22
-	je		_return			; jump to _return if ZF == 1
-	call	___error		; store ptr of variable errno to rax
-	mov		byte [rax], 22	; assign 22:EINVAL to errno
-	mov		rax, -1			; return value -1
-	jmp		_return
+	mov		byte [rax], r8b	; assign return value of syscall to errno
+	pop		r8				; restore data of r8
+	mov		rax, -1			; set -1 as return value
+	ret
