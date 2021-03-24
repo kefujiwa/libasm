071088ff05159aa422b644325fa8829756c151d6
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-25 03:05:04 +0900

    refactor(ft_strcpy.s): refactor ft_strcpy.s; create stack frame

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -17,13 +17,31 @@ global _ft_strcpy
 
 section .text
 _ft_strcpy:
-	mov		rcx, -1					; initialize counter to 0
+	push	rbp						; push rbp (base pointer) onto the stack
+	mov		rbp, rsp				; copy rsp (stack pointer) to rbp
+	mov		qword [rbp - 8], rdi	; copy rdi (dst) onto the stack
+	mov		qword [rbp - 16], rsi	; copy rsi (src) onto the stack
+	mov		qword [rbp - 24], rdi	; copy rdi (dst) onto the stack to save head of the ptr
 
-_loop:
-	inc		rcx						; increment counter
-	mov		al, byte [rsi + rcx]	; copy value of src[cnt] to al (8bit register)
-	mov		byte [rdi + rcx], al	; copy al to dst[cnt]
-	cmp		al, 0					; check if al is null character
-	jne		_loop					; jump to _loop if ZF == 0
-	mov		rax, rdi				; return dst
+loop:
+	mov		rax, qword [rbp - 16]	; copy src to rax
+	cmp		byte [rax], 0			; if (*src == 0)
+	je		return					; jump to _return if ZF == 1
+
+	mov		rcx, rax				; copy src to rcx
+	inc		rcx						; rcx++ (src++)
+	mov		qword [rbp - 16], rcx	; copy src onto the stack
+	mov		dl, byte [rax]			; copy [rax] (*src) to dl
+	mov		rax, qword [rbp - 8]	; copy dst to rax
+	mov		rcx, rax				; copy dst to rcx
+	inc		rcx						; rcx++ (dst++)
+	mov		qword [rbp - 8], rcx	; copy dst onto the stack
+	mov		byte [rax], dl			; copy *src to *dst
+	jmp		loop
+
+return:
+	mov		rax, qword [rbp - 8]	; copy dst to rax
+	mov		byte [rax], 0			; *dst = '\0'
+	mov		rax, qword [rbp - 24]	; return head of the ptr dst
+	pop		rbp
 	ret
